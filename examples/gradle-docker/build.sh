efbf8a8e3e1014f98f1d8ffccf075c401687b4ae
Mateusz Biel <mateusz.biel@f-secure.com>
2019-10-29 13:50:23 +0100

    update

--- examples/gradle-docker/build.sh
+++ examples/gradle-docker/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 docker run --mount type=bind,src=${PWD},dst=/workspace -w /workspace gradle:5.6.3-jdk gradle shadowJar
-docker build . -f Dockerfile.jre -t gradle-docker:0.1
+docker build -t gradle-docker:0.1 -f- . <<EOF
+FROM openjdk:8-jre-alpine3.9
+
+COPY build/libs/gradle-docker-all.jar /app/gradle-docker-all.jar
+CMD ["java", "-jar", "/app/gradle-docker-all.jar"]
+EOF
